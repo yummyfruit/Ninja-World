b8823494510ae0a271b2b2841edd012bbc4f503e
yummyfruit <166460176+yummyfruit@users.noreply.github.com>
2024-11-18 09:48:35 +1100

    18/11
    wokring on multiplayer

--- First Game/scripts/game_manager.gd
+++ First Game/scripts/game_manager.gd
@@ -7,3 +7,12 @@ var score = 0
 func add_point():
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins."
+
+func become_host():
+	print("Become host pressed")
+	%MultiplayerHud.hide()
+	MultiplayerManager.become_host()
+func join_as_player_2():
+	print("join as player 2")
+	%MultiplayerHud.hide()
+	MultiplayerManager.join_as_player_2() 
---
+++ First Game/scripts/multiplayer/multiplayer_manager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+func become_host():
+	print("Starting host!")
+	
+func join_as_player_2():
+	print("PLayer 2 joining")
