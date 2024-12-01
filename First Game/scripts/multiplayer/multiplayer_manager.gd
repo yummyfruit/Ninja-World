d0c15100b4418607f40b38d6275916535945201a
yummyfruit <166460176+yummyfruit@users.noreply.github.com>
2024-12-02 09:05:29 +1100

    Update multiplayer_manager.gd

--- First Game/scripts/multiplayer/multiplayer_manager.gd
+++ First Game/scripts/multiplayer/multiplayer_manager.gd
@@ -1,7 +1,20 @@
 extends Node
+const SERVER_PORT = 8080
+const SERVER_IP = "27.0.0.1"	
 
 func become_host():
 	print("Starting host!")
+	var server_peer = ENetMultiplayerPeer.new()
+	server_peer.create_server(SERVER_PORT)
+	multiplayer.multiplayer_peer = server_peer
+	
+	multiplayer.peer_connected.connect(_add_player_to_game)	
+	multiplayer.peer_disconnected.connect(_del_player)
+	
 	
 func join_as_player_2():
 	print("PLayer 2 joining")
+	
+	
+	
+func _add_player_to_game(id: int)
